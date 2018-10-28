84f3d49c6cf8c50a71e486cc16892d5fbdbf3195
lmcuadrado <42412749+lmcuadrado@users.noreply.github.com>
2018-10-27 22:37:37 -0700

    Add files via upload

---
+++ Fourier.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplolib.pylab as plt
+
+#--------------------- Punto 3.1----------------------------#
+signal=np.genfromtxt("signal.dat",delimiter=",")
+incompletos=np.genfromtxt("incompletos.dat",delimiter=",")
+
+#--------------------- Punto 3.2----------------------------#
+
